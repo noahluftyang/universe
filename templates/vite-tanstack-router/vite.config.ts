ac61ddabb31e02e73ae83f10b6097a05af279205
noahluftyang <euihyun.yang.9x@gmail.com>
2024-05-01 17:33:06 +0900

    feat: setup services

--- templates/vite-tanstack-router/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-});
